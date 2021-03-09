b1aaa394eae40ce45c5bf224f2321590045a239c
rgonzg17 <rgonzg17@estudiantes.unileon.es>
2021-03-09 20:42:30 +0100

    Primera actualización de la gráfica.
    Ya se redimensiona bien, y no es necesario maximizar para poder verla correctamente

--- src/cne/Ventana.java
+++ src/cne/Ventana.java
@@ -5,42 +5,46 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 
 public class Ventana extends JFrame {
-        JPanel panel;
-        public Ventana(Poblacion poblacion){
-            setTitle("Gráfica sobre evolución de algoritmo genético");
-            setSize(800,600);
-            setLocationRelativeTo(null);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-            setVisible(true);
-            init(poblacion);
-        }
+    JPanel panel;
 
-        private void init(Poblacion poblacion) {
-            panel = new JPanel();
-            getContentPane().add(panel);
-
-            // Fuente de Datos
-            DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
-            for (int i = 0; i < poblacion.getMaximoGeneraciones()-1; i++){
-                line_chart_dataset.addValue((int)poblacion.getAptitudesGeneracion().get(i), "aptitud", "generacion: "+i);
-            }
-
-
-            // Creando el Grafico
-            JFreeChart chart= ChartFactory.createLineChart("Evolucion aptitudes",
-                    "Generacion","Aptitudes",line_chart_dataset, PlotOrientation.VERTICAL,
-                    true,true,false);
-
-            // Mostrar Grafico
-            ChartPanel chartPanel = new ChartPanel(chart);
-            panel.add(chartPanel);
+    public Ventana(Poblacion poblacion) {
+        init(poblacion);
     }
 
-    public DefaultCategoryDataset valoresGrafica(DefaultCategoryDataset valores){
-            return valores;
+    private void init(Poblacion poblacion){
+        DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < poblacion.getMaximoGeneraciones() - 1; i++) {
+            line_chart_dataset.addValue((int) poblacion.getAptitudesGeneracion().get(i), "aptitud", "generacion: " + i);
+        }
+        JFreeChart chart = ChartFactory.createLineChart("Evolucion aptitudes",
+                "Generacion", "Aptitudes", line_chart_dataset, PlotOrientation.VERTICAL,
+                true, true, false);
+
+        ChartPanel panel = new ChartPanel(chart);
+        JFrame ventana = new JFrame("Algoritmo Genético");
+        ventana.getContentPane().add(panel);
+        ventana.pack();
+        ventana.setVisible(true);
+        ventana.setSize(800, 600);
+        ventana.setLocationRelativeTo(null);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public DefaultCategoryDataset valoresGrafica(DefaultCategoryDataset valores) {
+        return valores;
     }
 }
